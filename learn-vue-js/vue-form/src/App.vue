b70028b4d09a0e0231a412cc0676bf44639e86f6
Lee JinKyu <jaydeen@kakao.com>
2022-02-10 23:34:15 +0900

    init template

--- learn-vue-js/vue-form/src/App.vue
+++ learn-vue-js/vue-form/src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <div></div>
+  <form action="">
+    <div>
+      <label for="username">id: </label>
+      <input type="text" name="username" id="username">
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input type="password" name="password" id="password">
+    </div>
+    <button>login</button>
+  </form>
 </template>
 
 <script>
