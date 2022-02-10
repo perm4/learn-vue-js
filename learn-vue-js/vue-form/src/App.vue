766d38df9f6a0942f08ee3cd9be5d06d301f16d9
Lee JinKyu <jaydeen@kakao.com>
2022-02-10 23:48:34 +0900

    install axios, 데이터 전송 및 form 구현

--- learn-vue-js/vue-form/src/App.vue
+++ learn-vue-js/vue-form/src/App.vue
@@ -1,20 +1,45 @@
 <template>
-  <form action="">
+  <form v-on:submit.prevent="submitForm">
     <div>
       <label for="username">id: </label>
-      <input type="text" name="username" id="username">
+      <input type="text" id="username" v-model="username">
     </div>
     <div>
       <label for="password">pw: </label>
-      <input type="password" name="password" id="password">
+      <input type="password" id="password" v-model="password">
     </div>
-    <button>login</button>
+    <button type="submit">login</button>
   </form>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+  data: function() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    submitForm: function() {
+      // event.preventDefault();
+      console.log(this.username, this.password);
+      let url = 'https://jsonplaceholder.typicode.com/users';
+      let data = {
+        username: this.username,
+        password: this.password,
+      };
+      axios.post(url, data)
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+  },
 }
 </script>
 
